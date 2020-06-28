7fb6e96ff638c17453e1a30fad8e2eebe5fb112b
13901 <1390109557@qq.com>
2020-06-28 15:55:46 +0800

    简化

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-KwMusic 请求接口解密(逆向之后复制修改的源码)
+# KwMusic 请求接口解密(逆向之后复制修改的源码)
+
+## 使用方法
+打开http://www.kuwo.cn/
+搜索要找的歌曲例如
+<img src="https://github.com/jym66/socks-doc/blob/master/1.png" alt="logo" width="359" height="120">
--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,16 @@
 class Solution {
-    public static void main(String [] args){
-    String rid = "142655450";
-    System.out.println(getParams(rid));
+    public static void main(String[] args) {
+
+        String rid = "142655450";
+//        System.out.println(getParams(rid));
+        String Baseurl = "http://nmobi.kuwo.cn/mobi.s?f=kuwo&q=" + getParams(rid);
+        System.out.println(HttpUtil.doGet(Baseurl));
     }
-    public static String getParams(String rid){
-        String ss = "user=866174010595081&android_id=e846f77cb92a9941f&prod=kwplayer_ar_9.3.1.1&corp=kuwo&newver=2&vipver=9.3.1.1&source=kwplayer_ar_9.3.1.1_18.apk&p2p=1&notrace=0&type=convert_url2&br=2000kflac&format=mp3|aac&sig=0&rid="+rid+"&priority=bitrate&loginUid=0&network=WIFI&loginSid=0&mode=audition&uid=1371450462";
+
+    public static String getParams(String rid) {
+        String ss = "user=866174010595081&android_id=e846f77cb92a9941f&prod=kwplayer_ar_9.3.1.1&corp=kuwo&newver=2&vipver=9.3.1.1&source=kwplayer_ar_9.3.1.1_18.apk&p2p=1&notrace=0&type=convert_url2&br=2000kflac&format=mp3|aac&sig=0&rid=" + rid + "&priority=bitrate&loginUid=0&network=WIFI&loginSid=0&mode=audition&uid=1371450462";
         byte[] bytes = ss.getBytes();
-        byte[] a3 =d.a(bytes,bytes.length,d.f8946a);
-        return new String(b.a(a3,a3.length));
+        byte[] a3 = d.a(bytes, bytes.length, d.f8946a);
+        return new String(b.a(a3, a3.length));
     }
 }
